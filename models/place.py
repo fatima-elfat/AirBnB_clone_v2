01f49e74e53043281b7a8df9d3c97ed887713448
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-21 15:35:36 +0100

    change back_populate to backref

--- models/place.py
+++ models/place.py
@@ -55,7 +55,7 @@ class Place(BaseModel, Base):
         amenities = relationship("Amenity",
                                  secondary=place_amenity,
                                  viewonly=False,
-                                 back_populates="place_amenities")
+                                 backref="place_amenities")
 
     else:
         @property
