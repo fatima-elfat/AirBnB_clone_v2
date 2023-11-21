fda129ebd32820454d3c41732732bc04df4587a4
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-21 20:21:37 +0100

    place amenity pk

--- models/place.py
+++ models/place.py
@@ -11,10 +11,12 @@ place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id',
                              String(60),
                              ForeignKey('places.id'),
+                             primary_key=True,
                              nullable=False),
                       Column('amenity_id',
                              String(60),
                              ForeignKey('amenities.id'),
+                             primary_key=True,
                              nullable=False))
 
 
