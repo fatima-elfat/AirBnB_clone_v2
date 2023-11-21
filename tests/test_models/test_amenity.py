830de4615b107b9b82c544214fa890e18910d819
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-21 11:02:49 +0100

    add tests for Amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,51 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+""" ALL THE TESTS"""
+from os import getenv
+from models.base_model import BaseModel
 from models.amenity import Amenity
+import unittest
 
 
-class test_Amenity(test_basemodel):
-    """ """
+storage = getenv("HBNB_TYPE_STORAGE")
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+class test_Amenity(unittest.TestCase):
+    """ UNITTEST AMENIYTY"""
+    @classmethod
+    def setUp(self):
+        """SetUp method"""
+
+        self.amenity = Amenity()
+        self.new_amenity.name = "GYM"
+
+    @classmethod
+    def TearDown(self):
+        """TearDown method."""
+
+        del self.amenity
+
+    def test_type_object(self):
+        """Test type object of Amenity"""
+
+        self.assertEqual(
+            str(type(self.amenity)),
+            "<class 'models.amenity.Amenity'>")
+        self.assertIsInstance(self.amenity, Amenity)
+
+    def test_Amenity_inheritence(self):
+        """checks if inherits"""
+
+        self.assertIsInstance(self.new_amenity, BaseModel)
+
+    def test_db_tbname(self):
+        """checks the tablename"""
+
+        self.assertEqual(self.new_amenity.__tablename__, "amenities")
+
+    def test_attr(self):
+        """ checks the attributes"""
+
+        self.assertTrue("name" in self.new_amenity.__dir__())
+        if storage == 'db':
+            name_value = getattr(self.new_amenity, "name")
+            self.assertIsInstance(name_value, str)
