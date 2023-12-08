6e3146ce4539357d6ea89c9cbb87a7eecfe72232
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-08 02:13:34 +0100

    fix errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,11 +3,26 @@
 distributes an archive to your web servers,
 using the function do_deploy:
 """
+from time import strftime
 from fabric.api import env, put, run
 import os.path
 
+env.user = "ubuntu"
 env.hosts = ["52.86.154.130", "34.207.120.70"]
 
+
+def do_pack():
+    """
+    generates .tgz archive folder
+    """
+    try:
+        local("mkdir -p versions")
+        timefrmat = strftime("%Y%M%d%H%M%S")
+        filenme = "versions/web_static_{}.tgz".format(timefrmat)
+        local("tar -cvzf {} web_static/".format(filenme))
+        return filenme
+    except Exception:
+        return None
 
 def do_deploy(archive_path):
     """
@@ -20,23 +35,15 @@ def do_deploy(archive_path):
         nme = filenme.split(".")[0]
         path_r = "/data/web_static/releases/{}/".format(nme)
         path_c = "/data/web_static/current"
-        if put(archive_path, "/tmp/{}".format(filenme)).failed is True:
-            return False
+        put(archive_path, "/tmp/{}".format(filenme))
         """put(archive_path, "/tmp/")"""
-        if run("mkdir -p {}".format(path_r))is True:
-            return False
-        if run("tar -xzf /tmp/{} -C {}".format(filenme, path_r))is True:
-            return False
-        if run("rm /tmp/{}".format(filenme))is True:
-            return False
-        if run("mv {}web_static/* {}".format(path_r, path_r))is True:
-            return False
-        if run("rm -rf {}web_static".format(path_r))is True:
-            return False
-        if run("rm -rf {}".format(path_c))is True:
-            return False
-        if run("ln -s {} {}".format(path_r, path_c))is True:
-            return False
+        run("mkdir -p {}".format(path_r))
+        run("tar -xzf /tmp/{} -C {}".format(filenme, path_r))
+        run("rm /tmp/{}".format(filenme))
+        run("mv {}web_static/* {}".format(path_r, path_r))
+        run("rm -rf {}web_static".format(path_r))
+        run("rm -rf {}".format(path_c))
+        run("ln -s {} {}".format(path_r, path_c))
         return True
     except Exception:
         return False
