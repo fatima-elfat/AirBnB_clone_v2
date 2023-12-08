d292c9d01809b1c9f46dc1e7d033f6436edbbedb
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-08 02:44:40 +0100

    fix errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -44,4 +44,5 @@ def do_deploy(archive_path):
     run("rm -rf {}web_static".format(path_r))
     run("rm -rf {}".format(path_c))
     run("ln -s {} {}".format(path_r, path_c))
+    print('New version deployed!')
     return True
