5540e0e843837c7a2abd2be2fe5a118717adff53
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-10 20:05:12 +0100

    fix files

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,58 +1,39 @@
 #config
-include 'stdlib'
-$hbnb_static = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-}"
-
-
+# Install Nginx package
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-} ->
-
+}
+# Create directories for web_static
 file { '/data':
   ensure  => 'directory'
 } ->
-
 file { '/data/web_static':
   ensure => 'directory'
 } ->
-
 file { '/data/web_static/releases':
   ensure => 'directory'
 } ->
-
 file { '/data/web_static/releases/test':
   ensure => 'directory'
 } ->
-
+file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+# Set index.html file content
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "Holberton School\n"
-} ->
-
-file { '/data/web_static/shared':
-  ensure => 'directory'
-} ->
-
+}
+# Create symbolic link for web_static current
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-} ->
-
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/bin/:/usr/bin/:/usr/local/bin/'
 }
-
+# Set ownership for web_static directory
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
 file { '/var/www':
   ensure => 'directory'
 } ->
@@ -60,17 +41,28 @@ file { '/var/www':
 file { '/var/www/html':
   ensure => 'directory'
 } ->
-
 file { '/var/www/html/index.html':
   ensure  => 'present',
   content => "Holberton School\n"
 } ->
-
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+}
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $hbnb_static
-} ->
-
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+}"
+}
 exec { 'nginx restart':
   path => '/etc/init.d/'
 }
