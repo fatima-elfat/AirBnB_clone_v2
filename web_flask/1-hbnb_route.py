a2759eebed3b69dab926fac5e546595ff690428d
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-29 11:10:38 -0800

    fix requirment error

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,7 +2,9 @@
 """
 script that starts a Flask web application.
 """
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,5 +1,1 @@
-#!/usr/bin/python3
-"""Init flask """
-from flask import Flask
 
-app = Flask(__name__)
