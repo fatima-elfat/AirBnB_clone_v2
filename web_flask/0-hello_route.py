ba4faa1d093a396a6fb62ff4b33c39ba0d305ffa
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-29 19:33:01 +0100

    add file of task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application.
+"""
+from web_flask import app
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    display “Hello HBNB!”.
+    using the option strict_slashes=False.
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    """Your web application must be listening on 0.0.0.0
+    """
+    app.run(host="0.0.0.0")
---
+++ web_flask/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Init flask """
+from flask import Flask
+
+app = Flask(__name__)
