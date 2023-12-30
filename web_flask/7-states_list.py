ee9ade43b3c78805e9867824150e2be4f3a5415f
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-30 14:05:18 -0800

    add file of task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application.
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list/', strict_slashes=False)
+def states_list():
+    """
+    display a HTML page.
+    """
+    st = storage.all(State)
+    return flask.render_template('7-states_list.html', states=st)
+
+
+@app.teardown_appcontext
+def teardown_cntxt(exception):
+    """
+    teardown SQLAlchemy.
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Your web application must be listening on 0.0.0.0
+    """
+    app.run(host="0.0.0.0")
