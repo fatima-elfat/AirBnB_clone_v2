f002b4fe655174971045dc15cfce7c7c1265c4a7
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-30 12:02:38 -0800

    add file for task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    display “Hello HBNB!”.
+    using the option strict_slashes=False.
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    display “Hello HBNB!”.
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    display “C ” followed by the value of the text variable.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text):
+    """
+    display “Python ”, followed by the value of the text
+    variable (replace underscore _ symbols with a space)
+    """
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    """Your web application must be listening on 0.0.0.0
+    """
+    app.run(host="0.0.0.0")
