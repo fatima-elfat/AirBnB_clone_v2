644f7e06345f217005ffc88fa0e965cbc8ed4251
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-30 14:30:18 -0800

    add file of task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application.
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states/", strict_slashes=False)
+def states_cities_by_states():
+    """
+    display a HTML page.
+    """
+    st = storage.all("State")
+    return render_template("8-cities_by_states.html", states=st)
+
+
+@app.teardown_appcontext
+def teardown_cntxt(exception):
+    """
+    teardown SQLAlchemy.
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Your web application must be listening on 0.0.0.0
+    """
+    app.run(host="0.0.0.0")
