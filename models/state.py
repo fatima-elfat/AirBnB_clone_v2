537d6135334e391d83887f38708684c71ec3b87b
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-31 08:37:54 -0800

    fix pycodestyle errors

--- models/state.py
+++ models/state.py
@@ -16,9 +16,8 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City",
-                        backref="state",
-                        cascade="all, delete, delete-orphan")
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")
 
     @property
     def cities(self):
